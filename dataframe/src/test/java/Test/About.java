c954182cdeca406dbd0ef0425e14e1f0ef04f8b2
DELL <DELL@DESKTOP-4AC9D17>
2023-02-07 20:38:56 +0530

    About is added.

---
+++ dataframe/src/test/java/Test/About.java
@@ -0,0 +1,24 @@
+package Test;
+
+import java.io.IOException;
+
+import org.testng.annotations.Test;
+
+public class About {
+	
+	@Test 
+	public void logo5() throws IOException, InterruptedException
+	{
+	
+		System.out.println("Logo5 Displayed");
+		
+	}
+	@Test 
+	public void logo6() throws IOException, InterruptedException
+	{
+		
+		System.out.println("Logo6 Displayed");
+		
+	}
+
+}
